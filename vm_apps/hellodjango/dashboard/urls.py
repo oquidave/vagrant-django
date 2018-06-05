de7e7d2c4e9abd04c241da60be4ab6abf46ca708
mark <latimmark@gmail.com>
2018-06-05 13:12:55 +0300

    posha 2.0

--- vm_apps/hellodjango/dashboard/urls.py
+++ vm_apps/hellodjango/dashboard/urls.py
@@ -4,6 +4,7 @@ from dashboard import views
 urlpatterns = [
     url(r'^$', views.index, name='dashboard'),
     url(r'^sms/$', views.sms, name='sms'),
+    url(r'^profile/$', views.profile, name='profile'),
     url(r'^airtime/$', views.airtime, name='airtime_page'),
     url(r'^voice/$', views.voice, name='voice'),
     url(r'^payments/$', views.payments, name='payments_page'),
