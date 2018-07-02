1bd308d2d808f61555f3c7be0cfa8ddb4b15b2cd
ltmyth <latimmark@gmail.com>
2018-07-02 17:36:15 +0300

    with improves

--- vm_apps/hellodjango/payments/models.py
+++ vm_apps/hellodjango/payments/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Payhist(models.Model):
+	id = models.AutoField(primary_key=True)
+	date = models.DateTimeField(auto_now_add=True)
+	name = models.CharField(max_length=25)
+	amount = models.CharField(max_length=25)
+	status = models.CharField(max_length=25)
+	destination = models.CharField(max_length=25)
+	def __str__(self):
+		return self.amount
--- vm_apps/hellodjango/payments/views.py
+++ vm_apps/hellodjango/payments/views.py
@@ -1,13 +1,58 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 import africastalking
+import codecs
+import csv
 from django.http import HttpResponse
+from .models import Payhist
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 @login_required
 def index(request):
-	return render(request, "payments/index.html")
+	data = {}
+	if request.method == "POST":
+		#get name
+		name = request.POST.get("receiver_name")
+		
+		#get number
+		phoneNumber = request.POST.get("receiver_no")
+
+		#get amount
+		amount = request.POST.get("amount")
+		
+		#reason
+		reason = request.POST.get("mm_reason")
+
+		meta = {}
+
+		currencyCode = "UGX"
+
+		#consumers
+		recipient = [{
+		"name": name,
+        "phoneNumber": phoneNumber,
+        "currencyCode": currencyCode,
+        "amount": amount,
+        "reason": reason,
+        "metadata": {}
+    	}]
+		        
+		print(recipient)
+
+		sand_key ="db76dc5eb626a86afb261dc1eb729a5bd6c4c1ea04b5cec23162ae36f24bf377"
+		africastalking.initialize(username='sandbox', api_key=sand_key)
+		payment = africastalking.Payment
+		res = payment.mobile_b2c(product_name='0zz', consumers=recipient)
+		print(res)
+		#save history
+		pay_hist=Payhist(name=name,amount=currencyCode+amount,status="successful",destination=phoneNumber)
+		pay_hist.save()
+		stats = Payhist.objects.all()
+		print(stats)
+		return redirect('phistory')
+	else:
+		return render(request, "payments/index.html")
 
 
 @login_required
@@ -15,20 +60,28 @@ def bulk_pay(request):
 	data = {}
 	if request.method == "POST":
 		#get file content
-		file = request.FILES["receiver"]
+		csv_file = request.FILES["receiver"]
 
 		#reason
-		reason = request.POST.get("reason")
+		reason = request.POST.get("mm_reason")
 
 		#read into file
-		file_data = file.read().decode("utf-8")
+		file_data = csv_file.read().decode("utf-8")
+		
+		#split into lines
+		lines = file_data.split(',')
+		row = lines[:3]
+		row_data = [elem for elem in lines if elem != "\n"]
+		
+		
+		for row_data in lines:
+			print(row_data)			
 
-		print(file_data)
-
+		
 		sand_key ="db76dc5eb626a86afb261dc1eb729a5bd6c4c1ea04b5cec23162ae36f24bf377"
 		africastalking.initialize(username='sandbox', api_key=sand_key)
 		payment = africastalking.Payment
-		res = payment.mobile_b2c(product_name='Ozz', consumers=consumers)
+		res = payment.mobile_b2c(product_name='0zz', consumers=consumers)
 		print(res)
 		return redirect('phistory')
 	else:
@@ -36,7 +89,8 @@ def bulk_pay(request):
 
 @login_required
 def phistory(request):
-	return render(request, 'payments/phist.html')
+	users = Payhist.objects.all()
+	return render(request, 'payments/phist.html',{"users":users})
 
 @login_required
 def schedule_pay(request):
